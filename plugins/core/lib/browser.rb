9b3fae5794d8226ffd1b5c611c0823d2b36c695c
paulallton <paulallton@628a91ae-bb00-11de-82ee-51df2961d3cf>
2010-01-04 17:46:39 +0000

    first stab at cross platform browser
    git-svn-id: http://pmip.googlecode.com/svn/trunk@24 628a91ae-bb00-11de-82ee-51df2961d3cf

--- plugins/core/lib/browser.rb
+++ plugins/core/lib/browser.rb
@@ -1,6 +1,21 @@
+#Borrowed from: http://www.centerkey.com/java/browser/
+
+import java.lang.reflect.Method
+import java.lang.System
+
 class Browser
+  BROWSERS = ["firefox", "opera", "konqueror", "epiphany", "seamonkey", "galeon", "kazehakase", "mozilla", "netscape"]
+
   def open(url)
-    #TODO: currently windows specific
-    `rundll32 url.dll,FileProtocolHandler #{url}`
+    os_name = System.getProperty("os.name")
+    if os_name =~ /^Mac OS/
+      file_mgr = Class.forName("com.apple.eio.FileManager")
+      open_url = file_mgr.getDeclaredMethod("openURL", [String.class])
+      open_url.invoke(null, [url])
+    elsif os_name =~ /^Windows/
+      `rundll32 url.dll,FileProtocolHandler #{url}`
+    else
+      BROWSERS.each{|browser| return `#{browser} #{url}` unless `which #{browser}`.strip.empty? }
+    end
   end
 end

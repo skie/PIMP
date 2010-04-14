0f29073cd635bc572b13f22bb6927a0535cf17e6
paulallton <paulallton@628a91ae-bb00-11de-82ee-51df2961d3cf>
2010-04-14 17:11:20 +0000

    a little tidy up
    git-svn-id: http://pmip.googlecode.com/svn/trunk@73 628a91ae-bb00-11de-82ee-51df2961d3cf

--- plugins/core/lib/action/execute_command.rb
+++ plugins/core/lib/action/execute_command.rb
@@ -2,12 +2,12 @@ class ExecuteCommand < PMIPAction
   def initialize(command, path='', name = "#{self.class.to_s}: #{command}")
     super(name)
     @command = command
-    @path = path.gsub('/', "\\")
+    @path = path
   end
 
   def run(event, context)
     result('Running ...')
     #TODO: obviously this is windows only currently
-    Run.later { `start /D#{@path} #{@command}` }
+    Run.later { `start /D#{@path.to_s.gsub('/', "\\")} #{@command}` }
   end
 end
--- plugins/fxi/init.rb
+++ plugins/fxi/init.rb
@@ -4,4 +4,6 @@ bind 'ctrl F11', RunAntTarget.new('gwtCompile-src/java', 'Compile GWT')
 bind 'ctrl shift F11', RunAntTarget.new('deploy-gwt-css-and-images-from-intellij', 'Deploy CSS and Images')
 bind 'banana I', ExecuteConfiguration.new('GWT Investor Server (real services)', 'Run', 'Run Investor' )
 bind 'banana X', ExecuteCommand.new('fx.bat', context.root, 'Run FX.bat')
-bind 'banana R', OpenURL.new(context.filepath_from_root('/build/results/index.html'), 'View Unit Test Results')
+bind 'banana R', OpenURL.new(context.filepath_from_root('build/results/index.html'), 'View Unit Test Results')
+
+bind 'banana P', ExecuteCommand.new('regedit /s useUBSProxySettings.reg', context.filepath_from_root('tools'), 'Restore Browser Proxy')

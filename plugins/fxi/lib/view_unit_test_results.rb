7dfff5347e1a4a1bdba519055a57690580aa2340
paulallton <paulallton@628a91ae-bb00-11de-82ee-51df2961d3cf>
2010-04-23 15:56:17 +0000

    added support for method name navigation
    git-svn-id: http://pmip.googlecode.com/svn/trunk@78 628a91ae-bb00-11de-82ee-51df2961d3cf

--- plugins/fxi/lib/view_unit_test_results.rb
+++ plugins/fxi/lib/view_unit_test_results.rb
@@ -2,13 +2,19 @@
 class NavigateTo < PMIPServlet
   def get(request, response, context)
     if params.has_key?('class')
-      name = params['class']
-      results = Elements.new(context).find_class(name)
+      #TODO: params should be unencoded
+      class_name = params['class']
+      method_name = params['method'].nil? ? class_name : params['method']
+
+      pattern = /.*#{method_name}.*/
+      results = FindInFiles.new(Files.new(context).include("src/test-*/**/#{class_name}.java")).pattern(pattern, class_name)
+
       if results.size == 1
-        result(name + " -> " + results.first.getQualifiedName)
-        Navigator.new(context).open(results.first)
+        result = results.first
+        result(result.describe)
+        result.navigate_to(context)
       else
-        result("expected to find one class for: #{name}, but found: #{results}")
+        result("expected to find one class for: #{class_name}.#{method_name}, but found: #{results}")
       end
     end
   end
@@ -23,9 +29,9 @@ class UnitTestFailures < PMIPServlet
     #TODO: should be a HEREDOC
     content.sub!('</head>', "<script src=\"/assets/prototype.js\" type=\"text/javascript\"></script>
     <script language=\"javascript\" type=\"text/javascript\">
-	function goto(name){
+	function goto(class_name, method_name){
 		var url = \"http://localhost:#{port}/NavigateTo\";
-				var pars = \"class=\" + name + \"&timestamp=\" + new Date().getTime();
+				var pars = \"class=\" + class_name + \"&method=\" + method_name + \"&timestamp=\" + new Date().getTime();
 
 				var myAjax = new Ajax.Request(
 					url,
@@ -38,8 +44,13 @@ class UnitTestFailures < PMIPServlet
     </script>" + "\n</head>")
 
     content = content.split("\n").collect{|l|
-      if l =~ /^<td>(<a href="(.*?)">(.*?)<\/a>)<\/td>.*/
-        l.sub!($1, '<a title="open in IDE" href="javascript: void(0)" onclick="goto(\'' + $3 + '\')"><img border="0" src="/assets/openInIde.gif" align="center"></a>&nbsp;' + $1)
+      if l =~ /^<td><a href=".*?">(.*?)<\/a><\/td>.*(<a href="(.*?)#(.*?)">(.*?)<\/a>).*/
+        bit_to_replace = $2
+        class_name = $1
+        #TODO: this should be done in the servlet
+        #TODO: params should be encoded
+        method_name = $4.split(' ').first
+        l.sub!(bit_to_replace, "<a title=\"open in IDE\" href=\"javascript: void(0)\" onclick=\"goto('#{class_name}', '#{method_name}')\"><img border=\"0\" src=\"/assets/openInIde.gif\" align=\"center\"></a>&nbsp;" + bit_to_replace)
       end
       l
     }.join("\n")

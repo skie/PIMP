825d99e212949ee1d0a2f94a4544d413b7f9962d
paulallton <paulallton@628a91ae-bb00-11de-82ee-51df2961d3cf>
2010-06-09 16:25:15 +0000

    syncing
    git-svn-id: http://pmip.googlecode.com/svn/trunk@127 628a91ae-bb00-11de-82ee-51df2961d3cf

--- pmip/plugins/core/lib/binder.rb
+++ pmip/plugins/core/lib/binder.rb
@@ -1,25 +1,21 @@
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.KeyboardShortcut
 import com.intellij.openapi.keymap.KeymapManager
-import javax.swing.KeyStroke;
+import javax.swing.KeyStroke
 
 PMIP_MENU = "PMIP::PopupMenu"
 
-#TODO: consider holding onto all actions and keys - like mount, so they can be cleanly removed
 class Binder
   def self.bind(key, action, force)
     key.sub!('banana', 'ctrl alt shift')
-    key_map = KeymapManager.instance.active_keymap
     id = action.name
-    action_manager = ActionManager.instance
 
-    unregister(key, action, id, force, key_map, action_manager)
-    register(key, action, id, key_map, action_manager)
-    bind_menu(action, action_manager)
+    unregister(key, action, id, force)
+    register(key, action, id)
+    add_menu_item(action)
 
     key_binding = key == '' ? ' ' : " -> #{key} "
     puts "- Bound #{id}#{key_binding}#{render_usages(id)}"
-    self
   end
 
   private
@@ -32,17 +28,16 @@ class Binder
     KeyStroke.get_key_stroke(key)
   end
 
-  def self.bind_menu(action, action_manager)
-    pmip_action_group = action_manager.get_action(PMIP_MENU)
-    pmip_action_group.add(action)
+  def self.add_menu_item(action)
+    action_manager.get_action(PMIP_MENU).add(action)
   end
 
-  def self.unregister(key, action, id, force, key_map, action_manager)
+  def self.unregister(key, action, id, force)
     action_manager.unregister_action(id)
 
-    key_map.getActionIds(key_stroke(key)).each{|bound_id|
+    keymap.action_ids(key_stroke(key)).each{|bound_id|
       if force
-        key_map.remove_shortcut(bound_id, shortcut(key))
+        keymap.remove_shortcut(bound_id, shortcut(key))
       else
         if bound_id != id
           raise "- Unable to bind #{id} -> #{key}, because shortcut is already used by '#{bound_id}', to override use: 'bind [key_stroke], [action], {:force = true}'"
@@ -51,9 +46,17 @@ class Binder
     }
   end
 
-  def self.register(key, action, id, key_map, action_manager)
+  def self.register(key, action, id)
     action_manager.register_action(id, action)
-    key_map.add_shortcut(id, shortcut(key)) unless key == ''
+    keymap.add_shortcut(id, shortcut(key)) unless key == ''
+  end
+
+  def self.keymap
+    KeymapManager.instance.active_keymap
+  end
+
+  def self.action_manager
+    ActionManager.instance
   end
 
   #TOOD: remove duplication with mounter
---
+++ pmip/plugins/fxi/lib/static_import_type.rb
@@ -0,0 +1,45 @@
+class StaticImportType < PMIPAction
+  def initialize(known_types)
+    super()
+    @known_types = known_types
+  end
+
+  def run(event, context)
+    #TODO: make it work with inner classes/enums
+    type = context.editor_current_word
+    results = find_elements(type, context).collect{|t| t.qualified_name }.uniq
+
+    if results.empty?
+      result("could not find type to static import: #{type}")
+    elsif results.size > 1
+      result("expected to find one type for: #{type}, but found: #{results.join(', ')}")
+    else
+      result = results.first
+      result("added static import for: #{type}")
+      Refresh.file_system_after { mangle(context.editor_filepath, context.editor_current_line, type, result) }
+    end
+  end
+
+  private
+
+  def find_elements(type, context)
+    elements = Elements.new(context).find_class(type, true)
+    if !@known_types.has_key?(type)
+      return elements
+    else
+      return [elements.select{|t| t.qualifiedName == @known_types[type] }.first]
+    end
+  end
+
+  def mangle(file, line, type, qualified_type_name)
+    lines = file.readlines.collect{|l| remove_usage_of_type(l, type) }
+    lines.insert(2, "import static #{qualified_type_name}.*;")
+    file.writelines(lines)
+  end
+
+  def remove_usage_of_type(line, type)
+     return line if line =~ /^import/
+     return '' if line.strip == type
+     line.sub("#{type}.", '')
+  end
+end

bc1bac94f5a4af757648a27a04ffed7291cbf4f1
paulallton <paulallton@628a91ae-bb00-11de-82ee-51df2961d3cf>
2011-05-03 09:18:47 +0000

    removing fxi
    git-svn-id: http://pmip.googlecode.com/svn/trunk@190 628a91ae-bb00-11de-82ee-51df2961d3cf

--- pmip/plugins/fxi/lib/restart_investor.rb
+++
@@ -1,9 +0,0 @@
-class RestartInvestor < PMIPAction
-  def run(event, context)
-    Run.read_action {
-      Action.from_id('Stop').run(event.presentation)
-      RunConfiguration.new.choose('GWT Investor Server (real services)').run('Run', event.presentation)
-      result('done')
-    }
-  end
-end
